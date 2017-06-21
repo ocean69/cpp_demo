3de44f827ccedbbac8d7458b6571fa5f1ac7df72
DESKTOP-PLHOVI1\user2017_05_18 <1994018964@qq.com>
2017-06-21 21:22:22 +0800

    add enum class example

--- cpp_demo.cpp
+++ cpp_demo.cpp
@@ -5,6 +5,8 @@
 #include <string>
 #include <iostream>
 
+using namespace  std;
+
 namespace NameA
 {
 	
@@ -60,8 +62,6 @@ void demo1_exception_in_constructor()
 #include <vector>
 void list_initialization()
 {
-	using namespace std;
-	
 	int b = 1.2l;
 	//int a{ 1.2l }; //compile error
 	//short c{ b }; //compile error
@@ -74,23 +74,79 @@ void list_initialization()
 
 constexpr int constexpr_function_get_value()
 {
-
 	return 3 * 3;
 }
 
 void const_expression_bad_example()
 {
-	using namespace  std;
 	constexpr int value = constexpr_function_get_value();
 	int build_in_array[value];
 	cout << "array length: " << sizeof(build_in_array) / sizeof(build_in_array[0]) << endl;
 	switch (1) {
 	case value:
-		cout << value << endl;
+		cout << "constexpr can be used in case statement" << endl;
 		break;
 	default:
 		cout << "default" << endl;
 	}
+}
+
+void range_for()
+{
+	const string space{ " " };
+
+	int x[10]{ 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 };
+	for (int i : x) {
+		cout <<  i << space << ends;
+	}
+	cout << endl;
+
+	for (int& i : x) {
+		++i;
+	}
+
+	for (int i : x) {
+		cout << i << space << ends;
+	}
+	cout << endl;
+
+	for (auto i : { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9 }) {
+		cout << i << space << ends;
+	}
+	cout << endl;
+}
+
+
+
+void enum_class_example()
+{
+	enum class enum_class {
+		value1,
+		value2,
+		value3
+	};
+
+	enum class enum_class2 {
+		value1,
+		value2,
+		value3
+	};
+
+	enum old_enum {
+		value1,
+		value2,
+		value3
+	};
+	
+	//enum old_enum2 {
+	//value1, //error c2365, 'value1': redefinition
+	//value2,
+	//value3
+	//};
+
+	enum_class enum_class_value = enum_class::value1;
+	int a = value2; // old enum 
+	//int b = enum_class::value2; //enum class is a new type can't assign to int
 }
 
 int main()
@@ -98,6 +154,7 @@ int main()
 	demo1_exception_in_constructor();
 	list_initialization();
 	const_expression_bad_example();
+	range_for();
     return 0;
 }
 
